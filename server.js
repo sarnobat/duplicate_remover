ea2f6c330654c1f1c95bcf7985f6b75a04841481
Sridhar Sarnobat <ss401533@gmail.com>
2014-06-28 22:12:07 -0700

    Initial checkin

--- server.js
+++ server.js
@@ -10,18 +10,59 @@ var app = express();
 app.get('/dups/all', function(req, res) {
 	
 	var location = "/media/sarnobat/e/Sridhar/Photos/camera phone photos/iPhone/";
-	var type = "dups_excess_only.txt";
+	var type = "dups.txt";
+	console.log('1');
 	{
 		exec("cat '"+location+"/"+type+"'", function (error, stdout, stderr) { 
 			if (error != null) {
 				puts("ERROR:" + stderr);
 			}
+			console.log('2');
+			//puts(stdout);
 			var lines = stdout.split( "\n" )
 			puts(lines.length);
 			var json = {};
-			json[location] =lines ;
+			console.log('3');
+			var clustersInLocation = {};
+			var i = 0;
+			var end = false;
+			while (i < lines.length) {
+				console.log('4 - ' + lines[i]);
+				
+				if (/files in cluster/.test(lines[i])) {
+					console.log('5');
+					//clustersInLocation.push(lines[i]);
+					var cluster = [];
+					clustersInLocation[lines[i]] = cluster;
+					//cluster clustersInLocation[lines[i]];
+					i++;
+					
+					if (i >= lines.length || lines[i] == null) {
+						console.log('6');
+						break;
+					}
+					while (!(/files in cluster/.test(lines[i]))) {
+						console.log('7 - ' + i);
+						cluster.push({ "filesystem" : location + lines[i], "http" : "<not needed yet>" });
+						i++;
+						if (i >= lines.length) {
+							end = true;
+							break;
+						}
+					}
+				} else {
+					console.log('8');
+					i++;
+				}
+				if (end) {
+					break;
+				}
+				console.log('9');
+			}
 
-                        res.header("Access-Control-Allow-Origin", "*");
+			json[location] = clustersInLocation ;
+			console.log('10' + JSON.stringify(json, null, 4));
+			res.header("Access-Control-Allow-Origin", "*");			
 			res.send(json);
 		});
 	}
